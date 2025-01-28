604197fedc46b8e6879c21a24bcf027128a041e8
nyaniTz <cliffmlimwa@gmail.com>
2025-01-28 17:41:12 +0300

    second

--- send_email.php
+++ send_email.php
@@ -10,7 +10,7 @@ use PHPMailer\PHPMailer\Exception;
 function sendEmailWithAttachment($to, $subject, $message, $attachmentPath, $attachmentName) {
     $mail = new PHPMailer(true);
 
-    $subject = ' International Conference on Artificial Intelligence Horizons (AIH) (AIoT) | ' . $subject;
+    $subject = ' International Conference on Artificial Intelligence Horizons (AIH) | ' . $subject;
 
 
 
